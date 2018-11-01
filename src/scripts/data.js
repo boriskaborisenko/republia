e6e87a6de0c58bd16ccb38b1b79ccb3a1e381932
myNEWmac <boriskaborisenko@gmail.com>
2018-11-01 03:37:48 +0200

    go

---
+++ src/scripts/data.js
@@ -0,0 +1,81 @@
+var data = {
+    content:[
+        {tabs:[
+            {tab1:[
+                {left:"left content"},
+                {right:"right content"}
+            ]},
+        ]},
+        {products:[
+            {1:[
+                {text:'Republia Bank is a component of the economic model of decentralized Republia ecosystem, which combined principles of Central Banks and innovative technologies. Republia Bank automatically regulates demand and supply on stock exchanges and smooths volatility of RPB coin by balancing and automated analysis of the market situation.'},
+                {rel:'2019'},
+                {q:'Q1'},
+                {percent:100}
+            ]},
+            {2:[
+                {text:'Provides the expansion of the entire Republia ecosystem by interrelated projects, not by the addition of competitive Crowdfunding, thereby avoiding unfair investment companies.'},
+                {rel:'2020'},
+                {q:'Q2'},
+                {percent:100}
+            ]},
+            {3:[
+                {text:'RepubliaID is a digital-analog of Identity Card, the main feature of which is advanced security. Republia ID allows community members to influence the life of the ecosystem directly and ensures security through implementation into all parts of the global ecosystem. Identity Card that minimizes the risk of loss of funds or hacking user accounts.'},
+                {rel:'2019'},
+                {q:'Q1'},
+                {percent:100}
+            ]},
+            {4:[
+                {text:'Republia Smart Contract Platform is an easy-to-use tool for generating smart contracts, that allows to create, execute and manage smart contracts based on Republia blockchain by yourself.'},
+                {rel:'2020'},
+                {q:'Q3'},
+                {percent:100}
+            ]},
+            {5:[
+                {text:'Republia Cloud X is a secure storage of data and the future of the entire global ecosystem. It is a data cloud storage, where all user information is stored decentrally in an encrypted form.'},
+                {rel:'2021'},
+                {q:'Q4'},
+                {percent:100}
+            ]},
+            {6:[
+                {text:'Transaction will be made faster, than in such popular payment systems as MasterCard or Visa. <br><br>Provides speed in processing operations, opening/closing orders, as well as an ability to bind an internal bank card, using this card necessary amount of RPB coins will be sold according to market order and due to instantaneous operation of Atomic Swap mechanism.'},
+                {rel:'2019'},
+                {q:'Q4'},
+                {percent:100}
+            ]},
+            {7:[
+                {text:'Republia Wallet is a uniquely designed integrated technology, that includes both wallets, which solves the problem of heavy wallets with 16-digit addresses, and an instant messenger with the addition of contacts that provides secure exchange between users of messages, files (stored in Republia Cloud X) and also provides an opportunity to create smart contracts with contacts.'},
+                {rel:'2019'},
+                {q:'Q1'},
+                {percent:100}
+            ]},
+            {8:[
+                {text:'Republia follows the principle of “We-ecosystem”, where all users are of equal importance in the modernization of the network, so consensus Republia operates on algorithm Proof-of-Authority (PoA) + BFT (Byzantine Fault Tolerance).<br><br>A unique solution for coordinating changes in ecosystem conditions through direct user voting based on Republia blockchain, which, in turn, is based on “we-government” methodology.'},
+                {rel:'2019'},
+                {q:'Q2'},
+                {percent:100}
+            ]},
+        ]},
+        {roadmap:[
+            {"2018":[]},
+            {"2019":[]},
+            {"2020":[]},
+            {"2021":[]},
+            {"2022":[]},
+        ]},
+        {docs:[
+            {onepager:'https://media.republia.io/republia-one-pager-en.pdf'},
+            {overview:'https://media.republia.io/republia-overview-en.pdf'},
+            {whitepaper:'https://media.republia.io/republia-whitepaper-en.pdf'},
+            {techpaper:'https://media.republia.io/republia-techpaper-en.pdf'},
+            {finmodel:'https://docs.google.com/spreadsheets/d/1fqPKNz3uokD4sCHAZfRC3Gt2MlfqXEVxjoLVx5GWMEw/edit'},
+            {techcompetitive :'https://media.republia.io/republia-tech-comp-en.pdf'},
+            {ecosys:'https://media.republia.io/republia-tech-comp-en.pdf'},
+            {policy:'https://media.republia.io/republia-tech-comp-en.pdf'}
+        ]}
+    ]
+        
+};
+
+export {data};
+
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,126 @@
 import '../styles/index.scss';
+import {data} from './data';
 
+//console.log(data.content);
+/*
+var words = ['fastest','safest','scalability'];
 
+var typed = document.getElementById('fsc');
+var del = true;
+setInterval(()=>{
+    del ? clearText(typed) : addText(typed);
+}, 1000);
 
+function clearText(text){
+    if(text.innerHTML.length >= 0){
+        var newtext = text.innerHTML.substring(0, text.innerHTML.length - 1);
+        typed.innerHTML = newtext;
+    }else{
+        del = false;
+    }
+}
+
+function addText(text){
+    console.log(text.innerHTML.length);
+}
+*/
+
+var tabs = document.getElementsByClassName('tab_btn');
+var products = document.getElementsByClassName('one_prod'); 
+var videos = [
+    {id:'6ZIMqjYIFw4', title:'What is Republia?', desc:'Welcome to #Republia community!'},
+    {id:'FliKqP0JBfg', title:'Vladyslav Gyrych about Republia project', desc:'Many years of experience in the implementation...'},
+    {id:'E2cfGsOGtvc', title:'Daniel Shulyaev about Republia project', desc:'Extensive experience in developing technologies...'},
+    {id:'nGxLlvRwhrI', title:'Hayk Khachatryan about Republia project', desc:'Technical Director of Republia'},
+    {id:'DHvctzCl52o', title:'Speech by Daniel Shulyaev in the Ukrainian Parliament', desc:'Co-founder of Republia Daniel Shulyaev...'},
+    {id:'2NH4hNxIrUg', title:'Speech by Vladyslav Gyrych in the Ukrainian Parliament', desc:'Co-founder of Republia Vladyslav Gyrych...'}
+];
+
+document.getElementById('mainvid').innerHTML='<iframe id="vid_frame" src="https://www.youtube.com/embed/'+videos[0].id+'?rel=0&showinfo=0&autohide=1" width="560" height="315" frameborder="0" allowfullscreen></iframe>';
+videos.map(item => {
+    const vidslist = document.getElementById('videoslist');
+
+     vidslist.innerHTML +=  `<div class="list_video" onClick="document.getElementById('vid_frame').src='https://youtube.com/embed/${item.id}?autoplay=1&rel=0&showinfo=0&autohide=1&autoplay=1'">
+                        <div class="thumb inl_t" style="background:url(https://img.youtube.com/vi/${item.id}/0.jpg) center center no-repeat; background-size:cover;"></div>
+                        <div class="vid_desc inl_t">
+                          <div class="vid_title">${item.title}</div>
+                          <div class="vid_anno">${item.desc}</div>
+                        </div>
+                      </div>`;
+});
+
+[].forEach.call(tabs, function(element){
+    element.addEventListener('click',function(e){
+          e.preventDefault();
+          
+          console.log('click tab');
+          let active = document.querySelector('.active_tab');
+          active.classList.remove('active_tab'); //remove class  
+          this.classList.add('active_tab'); //add class to current click element
+    });
+});
+
+[].forEach.call(products, function(element){
+    element.addEventListener('click',function(e){
+          e.preventDefault();
+          
+          var id= Number(element.id.slice(5, 6));
+          const text = data.content[1].products[id-1][id][0].text;
+          const rel = data.content[1].products[id-1][id][1].rel;
+          const q = data.content[1].products[id-1][id][2].q;
+          const percent = data.content[1].products[id-1][id][3].percent;
+
+        
+          _('prodtext').fade('out', 400);
+          _('prodpercent').fade('out', 400);
+          _('prodline').fade('out', 400);
+          setTimeout(()=>{
+            document.getElementById('prodtext').innerHTML = text;
+            document.getElementById('prodline').style.width = percent+'%';
+            document.getElementById('prodpercent').innerHTML = percent+'%';
+            document.getElementById('prodq').innerHTML = q;
+            document.getElementById('proddate').innerHTML = rel;
+            _('prodtext').fade('in', 400);
+            _('prodpercent').fade('in', 400);
+            _('prodline').fade('in', 400);
+          },400);
+          
+
+          console.log('click prod', id);
+          let active = document.querySelector('.active_prod');
+          active.classList.remove('active_prod'); //remove class  
+          this.classList.add('active_prod'); //add class to current click element
+    });
+});
+
+function _(el) {
+    if (!(this instanceof _)) {
+      return new _(el);
+    }
+    this.el = document.getElementById(el);
+  }
+  
+  _.prototype.fade = function fade(type, ms) {
+    var isIn = type === 'in',
+      opacity = isIn ? 0 : 1,
+      interval = 50,
+      duration = ms,
+      gap = interval / duration,
+      self = this;
+  
+    if(isIn) {
+      //self.el.style.display = 'inline';
+      self.el.style.opacity = opacity;
+    }
+  
+    function func() {
+      opacity = isIn ? opacity + gap : opacity - gap;
+      self.el.style.opacity = opacity;
+  
+      //if(opacity <= 0) self.el.style.display = 'none'
+      if(opacity <= 0 || opacity >= 1) window.clearInterval(fading);
+    }
+  
+    var fading = window.setInterval(func, interval);
+  };
 
