777e0df50aea467400ea8d454aaaa529f6396741
myNEWmac <boriskaborisenko@gmail.com>
2018-11-21 14:10:56 +0200

    news add

---
+++ src/scripts/datanews.js
@@ -0,0 +1,39 @@
+//ONLY FIRST 3 TAGS will be displayed for each news
+const news = {
+        all:[
+                {
+                    title: "Mark your calendars with the upcoming events.",
+                    date: "20.11.2018",
+                    url: "https://www.facebook.com/2081421595457086/posts/2197228090543102/",
+                    hashtags: ["rebuplia", "article", "facebook"]
+                },
+                {
+                    title: "Public Sale was postponed to the 15th of January 2019.",
+                    date: "15.11.2018",
+                    url: "https://medium.com/@republia/public-sale-will-start-on-the-15th-of-january-2019-7595e85a414f",
+                    hashtags:["medium"]
+                },
+                {
+                    title: "The Republia project to create new major partnerships in Asia.",
+                    date: "14.11.2018",
+                    url: "https://www.facebook.com/2081421595457086/posts/2201545686778009/",
+                    hashtags: []
+                },
+                {
+                    title: "Republia shares the highlights of the Malta Blockchain Summit.",
+                    date: "5.11.2018",
+                    url: "https://medium.com/@republia/weekly-update-the-malta-blockchain-summit-d698f1bb95e3",
+                    hashtags: ["hashtag", "hashtag"]
+                },
+                {
+                    title: "The Republia team is excited to announce its fresh new website and logo.",
+                    date: "3.11.2018",
+                    url: "https://www.facebook.com/2081421595457086/posts/2195141980751713/",
+                    hashtags: ["rebuplia", "blockchain", "ocaml"]
+                },
+        
+            ]
+}
+
+export {news};
+
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,35 @@ import './videos';
 import './prods';
 import './animations';
 
+import {news} from './datanews';
+createNews(news.all);
+function createNews(item){
+    let newsblock = document.querySelector('.all_news');
+    item.map( el => {
+        let hashes = '';
+        el.hashtags.map((tag, i) =>{
+            if(i<3)
+            hashes += `<li>${tag}</li>`;
+        });
+
+        let block = `<div class="one_new">
+                  <div class="new_dot"></div>
+                  <div class="new_date">${el.date}</div>
+                  <div class="new_title_and_hashtags">
+                    <div class="new_title">${el.title}</div>
+                    <div class="hashtags">
+                      <ul>
+                        ${hashes}
+                      </ul>
+                    </div>
+                  </div>
+                  <a href="${el.url}" target="_blank" class="new_title_link"></a>
+                </div>`;
+        //console.log(block)        
+        newsblock.innerHTML += block;
+    });
+}
+
 
 
 const lang = navigator.language;
